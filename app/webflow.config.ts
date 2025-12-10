145266540c096145dd963f0d34ddc0bf2566845f
vortiff <icaro.orengo@gmail.com>
2025-12-10 14:36:56 +0100

    spostamento file config

--- app/webflow.config.ts
+++ app/webflow.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "@webflow/api";
 
 export default defineConfig({
-  name: "tannenliebe-map-backend",
-  type: "web-app",
-
-  bindings: {
-    DB: {
-      type: "database",
-      name: "tannenliebe_locations_db"
+  projects: {
+    "tannenliebe-map-backend": {
+      path: "/app",
+      type: "web-app",
     }
   }
 });
